ffb9fe130ce0764ffb43e03ecc0bb970e8f9e45b
p22anishiyama <p22anishiyama@fairfieldprep.org>
2022-01-31 10:53:29 -0500

    finished quiz

--- cycle-10_practice-quiz.py
+++ cycle-10_practice-quiz.py
@@ -1,22 +1,42 @@
 # Author: ATN 1/27/22
 
-def food_costs(groceries, costs):
-    # making sure that the original list will not be altered
-    groceries_mod = groceries
+# def food_costs(groceries, costs):
+#     # making sure that the original lists will not be altered
+#     groceries_mod = groceries
+#     costs_mod = costs
 
-    for i, v in enumerate(groceries):
-        groceries_mod[i] = "{0}: ${1}".format(groceries[i], costs[i])
-    return groceries_mod  
-        
+#     # for each nested list
+#     for x in groceries_mod:
+#         for i, v in enumerate(x):
+#             # formats each list entry to add the costs
+#             x[i] = v + ": $" + str(costs_mod[0])
+#             # remove the cost entry after using
+#             del(costs_mod[0])
+#     return groceries_mod
+       
 
-print(food_costs(['apple','pear','banana','salmon','tuna','cod','milk','eggs','yogurt'],[1.99,2.99,0.99,9.99,10.99,6.99,3.49,2.49,1.49]))
+# print(food_costs([['apple','pear','banana'],['salmon','tuna','cod'],['milk','eggs','yogurt']],[1.99,2.99,0.99,9.99,10.99,6.99,3.49,2.49,1.49]))
 
-def fibonacci (n):
-    x = 0
-    sequence = []
-    while x < n:
-        sequence.append
-        x += 1
-    return sequence
+# def factorial(n):
+#     counter = 1
+#     value = ""
 
-print(fibonacci(4))
+#     while counter < n + 1:
+#         value += str(counter)
+#         counter += 1
+#     return value
+
+# print(factorial(5))
+
+# def fibonacci (n):
+#     # setting the first two values as a starting point
+#     values = [0, 1]
+#     x = 0
+#     # setting a finite number of iterations
+#     while x < n:
+#         # adding the current value to the previous
+#         values.append(values[x] + values[-1])
+#         x += 1
+#     return values
+
+# print(fibonacci(10))
